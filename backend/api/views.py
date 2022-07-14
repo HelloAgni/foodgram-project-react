781f9804b32e4228e3c484656e794db7c679efa6
Байбак Кирилл <zloifox@gmail.com>
2022-07-14 20:46:52 +0300

    create data, install filters

--- backend/api/views.py
+++ backend/api/views.py
@@ -88,6 +88,11 @@ class CustomUserViewSet(UserViewSet):
             return UserCreateSerializer
         return UserListSerializer
 
+    def get_permissions(self):
+        if self.action == 'me':
+            self.permission_classes = [IsAuthenticated]
+        return super().get_permissions()
+
     @action(
         detail=False,
         permission_classes=(IsAuthenticated,))
